3d4df0d3f7bb766748daac20e385fb5391210d64
naveen <naveenarorakrnl@gmail.com>
2019-02-27 00:22:33 +0530

    made weather telegram bot

---
+++ learnbot.py
@@ -0,0 +1,79 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler,Filters,CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from weather import get_forecasts
+
+updater =Updater(token='657956136:AAEP4BJrQtNAQzstUsZiyhMmLpD8RMnoQ2Y')
+
+# allows to register handler -> command, text, video, audio, etc.
+dispatcher=updater.dispatcher
+
+def start(bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text='Hello, Welcome to my bot')
+#define a command handler
+start_handler=CommandHandler('start',start)
+dispatcher.add_handler(start_handler)
+
+def echo(bot,update):
+        bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+# create a text handler
+echo_handler= MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+def option(bot, update):
+        button=[
+                [
+                        InlineKeyboardButton('Option 1', callback_data='1'),
+                        InlineKeyboardButton('Option 2', callback_data='2'),
+                ],
+                [
+                        InlineKeyboardButton('Option 3', callback_data='3'),
+                        InlineKeyboardButton('Option 4', callback_data='4'),                        
+                ]
+        ]
+        reply_markup=InlineKeyboardMarkup(button)
+        bot.send_message(chat_id=update.message.chat_id,
+                        text="choose one option",
+                        reply_markup=reply_markup
+                        )
+option_handler=CommandHandler('option', option)
+dispatcher.add_handler(option_handler)
+
+def button(bot, update):
+        query=update.callback_query
+        bot.edit_message_text(chat_id=query.message.chat_id,
+                        text='Thanks for choosing {}'.format(query.data),
+                        message_id=query.message.message_id
+                        )
+button_handler= CallbackQueryHandler(button)
+dispatcher.add_handler(button_handler)
+
+
+def get_location(bot, update):
+        button=[
+                [
+                        KeyboardButton("Share Location", request_location=True)
+                ]
+        ]
+        reply_markup=ReplyKeyboardMarkup(button)
+        bot.send_message(chat_id=update.message.chat_id,
+                        text='Mind sharing location',
+                        reply_markup=reply_markup)
+
+get_location_handler=CommandHandler('location',get_location)
+dispatcher.add_handler(get_location_handler)
+
+def location(bot, update):
+        lat=update.message.location.latitude
+        lon=update.message.location.longitude
+        forecasts=get_forecasts(lat,lon)
+        bot.send_message(
+                chat_id=update.message.chat_id,
+                text=forecasts,
+                reply_markup=ReplyKeyboardRemove()
+        )
+location_handler=MessageHandler(Filters.location, location)
+dispatcher.add_handler(location_handler)
+
+
+
+updater.start_polling()
---
+++ weather.py
@@ -0,0 +1,34 @@
+from pyowm import OWM
+
+owm=OWM("3c90e695a941418f88b8178cf8a6ceab")
+
+def get_forecasts(lat,lon):
+    observation=owm.three_hours_forecast_at_coords(lat,lon)
+    forecasts = observation.get_forecast()
+    location=forecasts.get_location()
+    loc_name=location.get_name()
+    loc_lat=location.get_lat()
+    loc_lon=location.get_lon()
+    results=[]
+    for forecast in forecasts:
+        time=forecast.get_reference_time('iso')
+        status=forecast.get_status()
+        detailed=forecast.get_detailed_status()
+        temperature=forecast.get_temperature('celsius')
+        temp=temperature.get('temp')
+        temp_min=temperature.get('temp_min')
+        temp_max=temperature.get('temp_max')
+
+        results.append("""
+        location :{} lat :{} lon :{}
+        Time:{}
+        Status:{}
+        Detailed:{}
+        Temperature:{}
+        Min Temperature:{}
+        Mac Temperature:{}
+        """.format(loc_name, loc_lat, loc_lon, time, status, detailed, temp, temp_min, temp_max ))
+    return "".join(results[:10])
+
+if __name__=='__main__':
+    print(get_forecasts(-1.2, 36))
